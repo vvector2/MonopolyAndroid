9367c6d2cb820d7848548405fd4648fb2207edaf
MateuszHub <mateusz.n.mail@gmail.com>
2020-05-21 11:55:21 +0200

    icon change

--- MonopolyAndroid/source/components/Dice.js
+++ MonopolyAndroid/source/components/Dice.js
@@ -10,21 +10,23 @@ export const Dice = (props) => {
             anim,
             {
                 toValue: 100,
-                duration: 2000,
+                duration: 1500,
                 useNativeDriver: true,
             }
         ).start();
 
     }, [])
-    let maxX = Math.floor(props.offset + 320)
+    let maxX = Math.floor(props.offset + 200)
     
     let maxY = Math.floor(props.offset * 0.5 + 100)
+    let roll = Math.floor(Math.random() /10 + 0.85)
+    let spin = 5
     return (
         <Animated.View
             style={{
-                transform: [{ translateX: anim.interpolate({ inputRange: [0, 90, 100], outputRange: [400, -0.8 * maxX, -1 * maxX] })},
-                 { translateY: anim.interpolate({ inputRange: [0, 100], outputRange: [100, -1 * maxY] })},
-                { rotateZ: anim.interpolate({ inputRange: [0, 100], outputRange: ["0deg", "900deg"] }) },
+                transform: [{ translateX: anim.interpolate({ inputRange: [0, 60, 100], outputRange: [400, -1 * roll * maxX, -1 * maxX] })},
+                 { translateY: anim.interpolate({ inputRange: [0, 60, 100], outputRange: [100, -1 * roll * maxY,-1 * maxY] })},
+                { rotateZ: anim.interpolate({ inputRange: [0, 90, 100], outputRange: ["0deg", "0deg", `${spin}deg`] }) },
                 { perspective: 1000 }]
             }}>
             <Image style={{ width: 50, height: 50, position: "absolute" }} source={dices[i]}></Image>
